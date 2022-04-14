9ee18064d22773ac05a0c57a155cdb31ffd17c39
Nyituriki <igihozocolombe2003.com>
2022-04-14 11:52:18 -0700

    testing and swagger

--- test/article.js
+++ test/article.js
@@ -15,8 +15,8 @@ describe('Articles', () => {
            done();           
         });        
     });
-  describe('/GET book', () => {
-      it('it should GET all the books', (done) => {
+  describe('/GET article', () => {
+      it('it should GET all the article', (done) => {
             chai.request(server)
             .get('/article')
             .end((err, res) => {
@@ -29,14 +29,14 @@ describe('Articles', () => {
   });
   describe('/POST book', () => {
       it('it should not POST a book without pages field', (done) => {
-          let book = {
+          let article = {
               title: "The Lord of the Rings",
               body: "J.R.R. Tolkien",
               status: "active"
           }
             chai.request(server)
             .post('/article')
-            .send(book)
+            .send(article)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
@@ -45,14 +45,14 @@ describe('Articles', () => {
             });
       });
       it('it should POST a book ', (done) => {
-          let book = {
+          let article = {
               title: "The Lord of the Rings",
               body: "J.R.R. Tolkien",
               status: 1954
           }
             chai.request(server)
             .post('/article')
-            .send(book)
+            .send(article)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
@@ -66,13 +66,13 @@ describe('Articles', () => {
  /*
   * Test the /GET/:id route
   */
-  describe('/GET/:id book', () => {
+  describe('/GET/:id article', () => {
       it('it should GET a book by the given id', (done) => {
-          let book = new Article({ title: "The Lord of the Rings", body: "J.R.R. Tolkien", status: "active"});
-          book.save((err, book) => {
+          let article = new Article({ title: "The Lord of the Rings", body: "J.R.R. Tolkien", status: "active"});
+          article.save((err, book) => {
               chai.request(server)
-            .get('/article/' + book.id)
-            .send(book)
+            .get('/article/' + article.id)
+            .send(article)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
@@ -87,11 +87,11 @@ describe('Articles', () => {
       });
   });
   describe('/PUT/:id article', () => {
-    it('it should UPDATE a book given the id', (done) => {
-        let book = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-        book.save((err, book) => {
+    it('it should UPDATE a article given the id', (done) => {
+        let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
+        article.save((err, book) => {
               chai.request(server)
-              .put('/article/' + book.id)
+              .put('/article/' + article.id)
               .send({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
               .end((err, res) => {
                     res.should.have.status(200);
@@ -103,11 +103,11 @@ describe('Articles', () => {
     });
 });
 describe('/DELETE/:id book', () => {
-    it('it should DELETE a book given the id', (done) => {
-        let book = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-        book.save((err, book) => {
+    it('it should DELETE a article given the id', (done) => {
+        let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
+        article.save((err, article) => {
               chai.request(server)
-              .delete('/article/' + book.id)
+              .delete('/article/' + article.id)
               .end((err, res) => {
                     res.should.have.status(401);
                     res.body.should.be.a('object');
