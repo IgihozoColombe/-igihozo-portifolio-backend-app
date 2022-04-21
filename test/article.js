18aebc363c39836697642c8cccdff749ff361bb5
Nyituriki <igihozocolombe2003@gmail.com>
2022-04-21 12:17:23 +0200

    --- Auto-Git Commit ---

--- test/article.js
+++ test/article.js
@@ -23,15 +23,12 @@ describe('Articles', () => {
                   res.should.have.status(200);
                   res.body.should.be.a('array');
                   res.body.length.should.be.eql(0);
-                  res.body.should.have.property('title');
-                  res.body.should.have.property('body');
-                  res.body.should.have.property('status');
               done();
             });
       });
   });
-  describe('/POST book', () => {
-      it('it should not POST a book without pages field', (done) => {
+  describe('/POST article', () => {
+      it('it should not POST a article without pages field', (done) => {
           let article = {
               title: "The Lord of the Rings",
               body: "J.R.R. Tolkien",
@@ -47,7 +44,7 @@ describe('Articles', () => {
               done();
             });
       });
-      it('it should POST a book ', (done) => {
+      it('it should POST a article ', (done) => {
           let article = {
               title: "The Lord of the Rings",
               body: "J.R.R. Tolkien",
@@ -59,9 +56,9 @@ describe('Articles', () => {
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
-                  res.body.should.have.property('title');
-                  res.body.should.have.property('body');
-                  res.body.should.have.property('status');
+                //   res.body.article.should.have.property('title');
+                //   res.body.article.should.have.property('body');
+                //   res.body.article.should.have.property('status');
               done();
             });
       });
@@ -70,9 +67,9 @@ describe('Articles', () => {
   * Test the /GET/:id route
   */
   describe('/GET/:id article', () => {
-      it('it should GET a book by the given id', (done) => {
+      it('it should GET a article by the given id', (done) => {
           let article = new Article({ title: "The Lord of the Rings", body: "J.R.R. Tolkien", status: "active"});
-          article.save((err, book) => {
+          article.save((err, article) => {
               chai.request(server)
             .get('/article/' + article.id)
             .send(article)
@@ -82,7 +79,7 @@ describe('Articles', () => {
                   res.body.should.have.property('title');
                   res.body.should.have.property('body');
                   res.body.should.have.property('status');
-                  res.body.should.have.property('_id').eql(book.id);
+                  res.body.should.have.property('_id').eql(article.id);
               done();
             });
           });
@@ -92,7 +89,7 @@ describe('Articles', () => {
   describe('/PUT/:id article', () => {
     it('it should UPDATE a article given the id', (done) => {
         let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-        article.save((err, book) => {
+        article.save((err, article) => {
               chai.request(server)
               .put('/article/' + article.id)
               .send({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
@@ -105,7 +102,7 @@ describe('Articles', () => {
         });
     });
 });
-describe('/DELETE/:id book', () => {
+describe('/DELETE/:id article', () => {
     it('it should DELETE a article given the id', (done) => {
         let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
         article.save((err, article) => {
@@ -120,60 +117,3 @@ describe('/DELETE/:id book', () => {
     });
 });
 });
-// describe('Articles', () => {
-//     beforeEach((done) => {
-//         Article.deleteOne({}, (err) => { 
-//            done();           
-//         });        
-//     });
-//   describe('/GET article', () => {
-//       it('it should GET all the article', (done) => {
-//             chai.request(server)
-//             .get('/article')
-//             .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('array');
-//                   res.body.length.should.be.eql(0);
-//               done();
-//             });
-//       });
-//   });
-//   describe('/POST book', () => {
-//       it('it should not POST a book without pages field', (done) => {
-//           let article = {
-//               title: "The Lord of the Rings",
-//               body: "J.R.R. Tolkien",
-//               status: "active"
-//           }
-//             chai.request(server)
-//             .post('/article')
-//             .send(article)
-//             .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('object');
-                  
-//               done();
-//             });
-//       });
-//       it('it should POST a book ', (done) => {
-//           let article = {
-//               title: "The Lord of the Rings",
-//               body: "J.R.R. Tolkien",
-//               status: 1954
-//           }
-//             chai.request(server)
-//             .post('/article')
-//             .send(article)
-//             .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('object');
-//                 //   res.body.book.should.have.property('title');
-//                 //   res.body.book.should.have.property('body');
-//                 //   res.body.book.should.have.property('status');
-//               done();
-//             });
-//       });
-//   });
-
-// })
-
---
+++ test/user.js
@@ -0,0 +1,102 @@
+let mongoose = require("mongoose");
+let Article = require('../models/article');
+const User=require("../models/user")
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+
+describe('User', () => {
+    beforeEach((done) => {
+        User.deleteOne({}, (err) => { 
+           done();           
+        });        
+    });
+    describe('/GET user', () => {
+        it('it should GET all the user', (done) => {
+              chai.request(server)
+              .get('/user/users')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    // res.body.should.be.a('array');
+                    // res.body.length.should.be.eql(1);
+                done();
+              });
+        });
+    });
+  describe('/POST user', () => {
+      it('it should not login a user ', (done) => {
+          let user = {
+             firstname:"Ineza",
+             lastname:"Gaella",
+             username:"ganza",
+             email:"ganza@gmail.com",
+             password:"abanabeza"
+          }
+            chai.request(server)
+            .post('/user/signin')
+            .send(user)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  
+              done();
+            });
+      });
+      it('it should login a user ', (done) => {
+        let user = {
+            email:"ganza@gmail.com",
+            password:"abanabeza"
+         }
+            chai.request(server)
+            .post('/user/signin')
+            .send(user)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                
+              done();
+            });
+      });
+  });
+  describe('/POST user', () => {
+    it('it should not POST a user ', (done) => {
+        let user = {
+           email:"ganza@gmail.com",
+           password:"abanabeza"
+        }
+          chai.request(server)
+          .post('/user/signup')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                
+            done();
+          });
+    });
+    it('it should POST a user ', (done) => {
+      let user = {
+          firstname:"Ineza",
+          lastname:"Gaella",
+          username:"ganza",
+          email:"ganza@gmail.com",
+          password:"abanabeza"
+       }
+          chai.request(server)
+          .post('/user/signup')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+              
+            done();
+          });
+    });
+});
+
+})
+
