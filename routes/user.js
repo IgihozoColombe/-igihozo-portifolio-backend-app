ef7c23d8cee973ce68468a0eeee03f253c040bf7
Nyituriki <igihozocolombe2003@gmail.com>
2022-04-22 10:15:42 +0200

    --- Auto-Git Commit ---

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,57 @@ const express = require('express')
 const router = express.Router()
 const UserController=require("../controllers/user")
 
+/**
+ * @swagger
+ * definitions:
+ *  User: 
+ *    type: object  
+ *    properties:
+ * 
+ *      firstname:
+ *        type: string
+ *        description: The firstname  of the students
+ *        example: 'colombe'
+ *      lastname:
+ *        type: string
+ *        description: The lastname  of the students
+ *        example: 'igihozo'
+ *      email:
+ *        type: string
+ *        description: The email  of the students
+ *        example: 'igihozocolombe@gmail.com'
+ *      username:
+ *        type: string
+ *        description: The username  of the students
+ *        example: 'marie'
+ *      password:
+ *        type: string
+ *        description: The password  of the students
+ *        example: 'abanabeza'
+ */
+
+/**
+ * @swagger
+ * /user/signup:
+ *  post:
+ *   summary: Register User
+ *   description: create a new User
+ *   parameters:
+ *       - in: body
+ *         name: article
+ * 
+ *   requestBody:
+ *    content: 
+ *     application/json:
+ *      schema: 
+ *       $ref: '#/definitions/User'
+ *   responses:
+ *    200: 
+ *     description: User created successfully
+ *    500: 
+ *     description: There is an error in creating the User
+ */
+
 router.post("/signup",UserController.createUser)
 router.post('/signin',UserController.login)
 router.get('/users',UserController.getAllUsers)
