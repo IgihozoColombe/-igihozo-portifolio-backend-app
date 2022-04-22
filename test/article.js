09cfe7ff3746f6d1ee36b6474fe53e699ce50d78
Nyituriki <igihozocolombe2003@gmail.com>
2022-04-22 08:44:53 +0200

    --- Auto-Git Commit ---

--- test/article.js
+++ test/article.js
@@ -1,121 +1,65 @@
-let mongoose = require("mongoose");
-let Article = require('../models/article');
-const User=require("../models/user")
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../index');
-let should = chai.should();
+const chai = require('chai');
+    const chaiHttp = require('chai-http');
+    const should = chai.should();
+    var mongoose = require("mongoose");
 
+    // Import server
+    var server = require('../index');
 
-chai.use(chaiHttp);
+    // Import Todo Model
+    var Article = require("../models/article");
+    var User = require("../models/user");
 
-describe('Articles', () => {
-    beforeEach((done) => {
-        Article.deleteOne({}, (err) => { 
-           done();           
-        });        
-    });
-  describe('/GET article', () => {
-      it('it should GET all the article', (done) => {
+    // use chaiHttp for making the actual HTTP requests        
+    chai.use(chaiHttp);
+
+    describe('Article API', function() {
+
+        it('should Register user, login user, check token and delete a article on /article/<id> DELETE', function(done) {
             chai.request(server)
-            .get('/article')
-            .set('Authorization', 'JWT ' + token)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('array');
-                  res.body.length.should.be.eql(0);
-              done();
-            });
-      });
-  });
-  describe('/POST article', () => {
-      it('it should not POST a article without pages field', (done) => {
-          let article = {
-              title: "The Lord of the Rings",
-              body: "J.R.R. Tolkien",
-              status: "active"
-          }
-            chai.request(server)
-            .post('/article')
-            .send(article)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  
-              done();
-            });
-      });
-      it('it should POST a article ', (done) => {
-          let article = {
-              title: "The Lord of the Rings",
-              body: "J.R.R. Tolkien",
-              status: 1954
-          }
-            chai.request(server)
-            .post('/article')
-            .send(article)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                //   res.body.article.should.have.property('title');
-                //   res.body.article.should.have.property('body');
-                //   res.body.article.should.have.property('status');
-              done();
-            });
-      });
-  });
- /*
-  * Test the /GET/:id route
-  */
-  describe('/GET/:id article', () => {
-      it('it should GET a article by the given id', (done) => {
-          let article = new Article({ title: "The Lord of the Rings", body: "J.R.R. Tolkien", status: "active"});
-          article.save((err, article) => {
-              chai.request(server)
-            .get('/article/' + article.id)
-            .send(article)
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  res.body.should.have.property('title');
-                  res.body.should.have.property('body');
-                  res.body.should.have.property('status');
-                  res.body.should.have.property('_id').eql(article.id);
-              done();
-            });
-          });
 
-      });
-  });
-  describe('/PUT/:id article', () => {
-    it('it should UPDATE a article given the id', (done) => {
-        let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-        article.save((err, article) => {
-              chai.request(server)
-              .put('/article/' + article.id)
-              .send({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-              
-                done();
-              });
-        });
-    });
-});
-describe('/DELETE/:id article', () => {
-    it('it should DELETE a article given the id', (done) => {
-        let article = new Article({title: "The Chronicles of Narnia", body: "J.R.R. Tolkien", status: "pending"})
-        article.save((err, article) => {
-              chai.request(server)
-              .delete('/article/' + article.id)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    
-                done();
-              });
-        });
-    });
-});
-});
+                // register request
+                .post('/user/signup')
+
+                // send user registration details
+                .send({
+                    "firstname":"Ineza",
+                    "lastname":"Gaella",
+                    "username":"ganza",
+                    "email":"tester@gmail.com",
+                    "password":"tester"
+                    }
+
+                ) // this is like sending $http.post or this.http.post in Angular
+                .end((err, res) => { // when we get a resonse from the endpoint
+
+                    // in other words,
+                    // the res object should have a status of 201
+                    res.should.have.status(201);
+
+                    // follow up with login
+                    chai.request(server)
+                        .post('/user/signin')
+                        // send user login details
+                        .send({
+                            'email': 'tester@gmail.com',
+                            'password': 'tester'
+                        })
+                        .end((err, res) => {
+                            console.log('this runs the login part');
+                            res.body.should.have.property('token');
+                            var token = res.body.token;
+
+                            // follow up with requesting user protected page
+                            chai.request(server)
+                                .get('/article')
+                                .set('Authorization', 'JWT ' + token)
+                                .end(function(err, res) {
+                                    chai.request(server)
+                                            done();
+                                        });
+                                })
+                        })
+                })
+        })
+  
